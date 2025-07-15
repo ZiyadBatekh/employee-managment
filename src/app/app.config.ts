6a8ebea72a638b533c94c89982bdef87b84c9da0
ZiyadBatekh <z.batikh111@gmail.com>
2025-07-15 17:46:17 +0300

    Add Transloco for internationalization, update dependencies and add i18n translation files for ar and en

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,20 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideTransloco, provideTranslocoConfig, provideTranslocoLoader } from '@ngneat/transloco';
+import { TranslocoHttpLoader } from './shared/services/transloco.loader';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslocoConfig({
+      availableLangs: ['en', 'ar'],
+      defaultLang: 'en',
+      reRenderOnLangChange: true,
+      prodMode: false
+    }),
+    provideTranslocoLoader(TranslocoHttpLoader)
+  ]
 };
